d4d0de96a3cb51613ffa8a72e674497f9a252b55
papycoda <opeyemi655@gmail.com>
2021-02-24 16:33:45 +0100

    fix deploy

--- master/pms/settings.py
+++ master/pms/settings.py
@@ -151,7 +151,7 @@ MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 try:
     from pms.local_settings import *
