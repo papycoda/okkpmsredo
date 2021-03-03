4ee8291df359e5c15bbc6b913b014ea07483c59e
papycoda <opeyemi655@gmail.com>
2021-03-03 13:11:44 +0100

    fix deploy

--- master/pms/settings.py
+++ master/pms/settings.py
@@ -151,7 +151,7 @@ MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 try:
     from pms.local_settings import *
@@ -159,4 +159,5 @@ except ImportError:
     pass
 
 # Activate Django-Heroku.
+import django_heroku
 django_heroku.settings(locals())
