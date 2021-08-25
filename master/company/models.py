b96e8ca3e9cb079598ef62b342215a0802d0236e
Papycoda <opeyemi655@gmail.com>
2021-08-25 15:25:33 +0100

    models

--- master/company/models.py
+++ master/company/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Branch(models.Model):
     branch_name = models.CharField(max_length = 255)
+    branch_id = models.IntegerField(default=1)
 
     def __str__(self):
         return self.branch_name
